eac8db11c23efa7a869ab4ec6c484bfce68b82a0
Greg Terrono <gterrono@seas.upenn.edu>
2013-01-17 01:29:44 -0500

    Removed unused files

--- lib/classes/errors.rb
+++
@@ -1,6 +0,0 @@
-#These errors serve as more specific exceptions so we know where exactly errors are coming from.
-class CourseError < StandardError
-end
-
-class InstructorError < StandardError
-end
