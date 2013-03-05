57b7eed940e580e525dbf67abedf4bbf792aeb01
Greg Terrono <gterrono@seas.upenn.edu>
2013-03-04 23:45:24 -0500

    Added a list of rating keys

--- lib/pcr/resource.rb
+++ lib/pcr/resource.rb
@@ -1,5 +1,16 @@
 module PCR
   module Resource
+    def rating_keys
+      %w(rCourseQuality rInstructorQuality rDifficulty rCommAbility
+         rInstructorAccess rReadingsValue rAmountLearned rWorkRequired
+         rRecommendMajor rRecommendNonMajor rStimulateInterest
+         rArticulateGoals rSkillEmphasis rHomeworkValuable
+         rExamsConsistent rAbilitiesChallenged rClassPace
+         rOralSkills rInstructorConcern rInstructorRapport
+         rInstructorAttitude rInstructorEffective rGradeFairness
+         rNativeAbility rTAQuality)
+    end
+
     def set_attrs(attrs, json)
       attrs.each do |attr|
         self.instance_variable_set("@#{attr}", json['result'][attr] || json[attr])
